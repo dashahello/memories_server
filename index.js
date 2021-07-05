d3e08a15ef094e106a1bbede27b4e917c4b4c3c5
dashahello <st.daryaa@gmail.com>
2021-07-05 23:36:30 +0300

    + env file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-//
+import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
 
 const app = express();
@@ -11,14 +11,13 @@ app.use(express.urlencoded({ limit: '30 mb', extended: true }));
 app.use(cors());
 
 app.use('/posts', postRoutes);
+dotenv.config();
 
-const CONNECTION_URL =
-  'mongodb+srv://dasha:12345Test@local-library.3mybs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 // Conection to db
 mongoose
-  .connect(CONNECTION_URL, {
+  .connect(process.env.CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
